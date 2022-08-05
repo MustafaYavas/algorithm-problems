63896d603c44f8842497722186cbe857ac834f23
MustafaYavas <mustafayavas40@gmail.com>
2022-08-05 19:06:14 +0300

    Add new problems

--- GradingStudents.js
+++ GradingStudents.js
@@ -2,7 +2,7 @@ grades = [73, 67, 38, 33];
 
 const gradingStudents = (grades) => {
     let newGrades = [];
-    for(let i=0; i< grades.length; i++) {
+    for(let i=0; i<grades.length; i++) {
         if(grades[i] >= 38) {
             let base = Math.floor(grades[i]/5);
             if((base+1)*5 - grades[i] < 3) newGrades.push((base+1)*5);
@@ -15,13 +15,3 @@ const gradingStudents = (grades) => {
 }
 
 console.log(gradingStudents(grades));
-
-/*
-if(grades[i] < 38) {
-            newGrades.push(grades[i]);
-            continue;
-        }
-        let base = Math.floor(grades[i]/5);
-        if((base+1)*5 - grades[i] < 3) newGrades.push((base+1)*5);
-        else newGrades.push(grades[i]);
-*/
