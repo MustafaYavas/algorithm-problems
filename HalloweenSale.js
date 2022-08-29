57d008e9e7c132d298d140da7d6b87ca7001f5ff
MustafaYavas <mustafayavas40@gmail.com>
2022-08-29 11:20:36 +0300

    Add new problem

---
+++ HalloweenSale.js
@@ -0,0 +1,18 @@
+const howManyGames = (p, d, m, s) => {
+    let totalAmount = 0;
+    let gamesCount = 0;
+
+    while(totalAmount < s) {
+        totalAmount += p;
+        gamesCount++;
+        
+        if(p-d < m) p = m
+        else p -= d;
+        
+        if(totalAmount > s) gamesCount--;
+    }
+
+    return gamesCount;
+}
+
+console.log(howManyGames(20, 3, 6, 70));
